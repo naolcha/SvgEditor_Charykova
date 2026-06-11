bc6a8f36f227b173a476d3c38fd53622dee56516
naolcha <ohnocringen@gmail.com>
2026-06-11 17:52:47 +0300

    Add basic SVG document model

---
+++ SvgIconEditor.Core/Class1.cs
@@ -0,0 +1,8 @@
+namespace SvgIconEditor.Core;
+
+public class SvgDocument
+{
+    public int Width { get; set; } = 1200;
+    public int Height { get; set; } = 800;
+    public string BackgroundColor { get; set; } = "#FFFFFF";
+}
